93bbf6d0b1358679381cb8c0daf312081aad63e1
Andrew <werdnaonaps@gmail.com>
2015-01-16 02:34:12 -0500

    created player prototype

--- game.js
+++ game.js
@@ -2,11 +2,18 @@
 var gameHeight = 600,
     gameWidth = 800;
 
-var player;
-
 var game = new Phaser.Game(gameWidth, gameHeight, Phaser.CANVAS, '', {
     preload: preload, create: create, update: update });
 var cursor;
+
+//prototype for the player
+
+function player(sprite) {
+    this.sprite = sprite;
+
+}
+
+
 function preload () {
     game.load.image('player', 'assets/images/player.png');
     game.load.image('enemy', 'assets/images/enemy.png');
@@ -22,36 +29,36 @@ function create() {
     
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.stage.backgroundColor = '#2d2d2d';
-    player = game.add.sprite(gameWidth -32,gameHeight - 32, 'player');
-    game.physics.arcade.enable(player);
+    player = new player(game.add.sprite(gameWidth-32, gameHeight -32, 'player'));
+    game.physics.arcade.enable(player.sprite);
     cursor = game.input.keyboard.createCursorKeys();
-    player.body.collideWorldBounds = true;
+    player.sprite.body.collideWorldBounds = true;
     
 }
 
 function update() {
-    player.body.velocity.x = 0;
-    player.body.velocity.y = 0;
+    player.sprite.body.velocity.x = 0;
+    player.sprite.body.velocity.y = 0;
     movePlayer();
     
 }
 
 function movePlayer() {
     if (cursor.left.isDown) {
-        player.body.velocity.x = -150;
+        player.sprite.body.velocity.x = -150;
         //player.animations.play('left');
     }
     else if (cursor.right.isDown) {
-        player.body.velocity.x = 150;
-        //player.animations.play('right');
+        player.sprite.body.velocity.x = 150;
+        //player.sprite.animations.play('right');
     }
     else if (cursor.up.isDown) {
         //Move up the screen
-        player.body.velocity.y = -150;
+        player.sprite.body.velocity.y = -150;
     }
     else if (cursor.down.isDown) {
         //Move down the screen
-        player.body.velocity.y = 150;
+        player.sprite.body.velocity.y = 150;
     }
     
 }
