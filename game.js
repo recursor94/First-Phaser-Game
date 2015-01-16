682429b68dcc7cea75ad2acf74204a566686cf3d
Andrew <werdnaonaps@gmail.com>
2015-01-16 18:07:13 -0500

    renamed enemy entities

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+
+
+
 //configure height and width of game window
 var gameHeight = 600,
     gameWidth = 800;
@@ -7,12 +10,13 @@ var walls;
 var game = new Phaser.Game(gameWidth, gameHeight, Phaser.CANVAS, '', {
     preload: preload, create: create, update: update });
 var cursor;
-var characters;
-var enemies;
-
+var enemyEntities;
+var level = 0;
+var player;
 //prototype for the player
 
 function Player(spritePath) {
+    this.sprite;
 
     this.preload = function() {
 	game.load.image('player', spritePath);
@@ -21,18 +25,40 @@ function Player(spritePath) {
     };
 
     this.create = function() {
-	player = game.add.sprite(game.width-170, game.height -32, 'player');
-	game.physics.arcade.enable(player);
-	player.body.collideWorldBounds = true;
+	this.sprite = game.add.sprite(game.width-170, game.height -32, 'player');
+	game.physics.arcade.enable(this.sprite);
+	player.sprite.body.collideWorldBounds = true;
     };
 
     this.update = function() {
-	player.body.velocity.x = 0;
-	player.body.velocity.y = 0;
+	this.sprite.body.velocity.x = 0;
+	this.sprite.body.velocity.y = 0;
 	movePlayer();    
     };
     
 
+
+};
+function enemies (spritePath) {
+    
+    this.group = game.add.group();
+    this.preload = function() {
+	game.load.image('enemy', spritePath);
+    }
+
+    this.create = function() {
+	this.group.createMultiple(10, 'enemy');
+	this.group.enableBody=true;
+	this.group.physicsBodyType = Phaser.Physics.ARCADE;
+
+
+    }
+
+    this.update = function () {
+	var enemy = this.group.getFirstExists(false);
+	enemy.reset(0,0);
+	
+    }
 
 }
 
@@ -48,6 +74,10 @@ function Walls (spritePath) {
 
 
 function preload () {
+    player= new Player('assets/images/player.png');
+    enemyEntities = new enemy('assets/images/enemy.png')
+    
+    player.preload();
 
     game.load.image('ally', 'assets/images/ally.png');
     game.load.image('wall', 'assets/images/wall.png');
@@ -58,9 +88,8 @@ function preload () {
 
 
 function create() {
-    
-
-    
+    player.create();
+    enemyEntities.create();
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.stage.backgroundColor = '#2d2d2d';
 
@@ -78,23 +107,17 @@ function create() {
 
     //enemy code
     
-    enemies =  game.add.group();
-    enemies.enableBody=true;
-    enemies.physicsBodyType = Phaser.Physics.ARCADE;
-    var enemy = enemies.create(game.width-500, game.height-200, 'enemy');
+ 
     
     
 }
 
 function update() {
-    game.physics.arcade.collide(player, wall);
-    game.physics.arcade.collide(enemies, wall);
-    //player.collideWorldBounds = true;
+    game.physics.arcade.collide(player.sprite, walls);
+    game.physics.arcade.collide(enemyEntities, walls);
 
-
-
-
-
+    player.update();
+    enemyEntities.update();
     
 }
 
@@ -102,11 +125,12 @@ function movePlayer() {
   var velocityModifier = 500;
    if (cursor.up.isDown) {
         //Move up the screen
-        player.body.velocity.y = -velocityModifier;
+        player.sprite.body.velocity.y = -velocityModifier;
     }
     else if (cursor.down.isDown) {
         //Move down the screen
-        player.body.velocity.y = velocityModifier;
+        player.sprite.body.velocity.y = velocityModifier;
     }
     
 }
+
