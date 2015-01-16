ebc56eb8a2d09730379a8dadc3eca225af66a909
Andrew <werdnaonaps@gmail.com>
2015-01-16 12:24:50 -0500

    repositioned player and wall

--- game.js
+++ game.js
@@ -38,11 +38,11 @@ function create() {
     
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.stage.backgroundColor = '#2d2d2d';
-    player = game.add.sprite(gameWidth-32, gameHeight -32, 'player');
+    player = game.add.sprite(game.width-170, game.height -32, 'player');
     wall = game.add.group();
     wall.enableBody=true;
     wall.physicsBodyType = Phaser.Physics.ARCADE;
-    var brick = wall.create(550,0, 'wall');
+    var brick = wall.create(game.width-200,0, 'wall');
     brick.scale.setTo(1,24);
     brick.body.immovable=true;
     
